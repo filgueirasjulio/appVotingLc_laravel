3ee3cbf1c146e910ac1e9422271aa3a5c7a3a807
Julio Filgueiras <julio_filgueiras@live.com>
2021-09-22 01:26:25 -0300

    test: CategoryFiltersTest

--- tests/Feature/StatusFiltersTest.php
+++ tests/Feature/StatusFiltersTest.php
@@ -2,15 +2,16 @@
 
 namespace Tests\Feature;
 
-use App\Http\Livewire\StatusFilters;
-use App\Models\Category;
-use App\Models\Idea;
-use App\Models\Status;
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Livewire\Livewire;
 use Tests\TestCase;
+use App\Models\Idea;
+use App\Models\User;
+use App\Models\Status;
+use Livewire\Livewire;
+use App\Models\Category;
+use App\Http\Livewire\IdeasIndex;
+use App\Http\Livewire\StatusFilters;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class StatusFiltersTest extends TestCase
 {
@@ -88,53 +89,60 @@ class StatusFiltersTest extends TestCase
             ->assertSee('Implemented (2)');
     }
 
-    /** @test */
-    public function filtering_works_when_query_string_in_place()
-    {
-        $user = User::factory()->create();
-
-        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
-
-        $statusOpen = Status::factory()->create(['name' => 'Open']);
-        $statusConsidering = Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
-        $statusInProgress = Status::factory()->create(['name' => 'In Progress', 'classes' => 'bg-yellow text-white']);
-        $statusImplemented = Status::factory()->create(['name' => 'Implemented']);
-        $statusClosed = Status::factory()->create(['name' => 'Closed']);
-
-        Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusConsidering->id,
-        ]);
-
-        Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusConsidering->id,
-        ]);
-
-        Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusInProgress->id,
-        ]);
-
-        Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusInProgress->id,
-        ]);
-
-        Idea::factory()->create([
-            'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
-            'status_id' => $statusInProgress->id,
-        ]);
-
-        $response = $this->get(route('idea.index', ['status' => 'In Progress']));
-        $response->assertSuccessful();
-        $response->assertSee('<div class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">In Progress</div>', false);
-        $response->assertDontSee('<div class="bg-purple text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Considering</div>', false);
+      /** @test */
+      public function filtering_works_when_query_string_in_place()
+      {
+          $user = User::factory()->create();
+  
+          $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+  
+          $statusOpen = Status::factory()->create(['name' => 'Open']);
+          $statusConsidering = Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
+          $statusInProgress = Status::factory()->create(['name' => 'In Progress', 'classes' => 'bg-yellow text-white']);
+          $statusImplemented = Status::factory()->create(['name' => 'Implemented']);
+          $statusClosed = Status::factory()->create(['name' => 'Closed']);
+  
+          Idea::factory()->create([
+              'user_id' => $user->id,
+              'category_id' => $categoryOne->id,
+              'status_id' => $statusConsidering->id,
+          ]);
+  
+          Idea::factory()->create([
+              'user_id' => $user->id,
+              'category_id' => $categoryOne->id,
+              'status_id' => $statusConsidering->id,
+          ]);
+  
+          Idea::factory()->create([
+              'user_id' => $user->id,
+              'category_id' => $categoryOne->id,
+              'status_id' => $statusInProgress->id,
+          ]);
+  
+          Idea::factory()->create([
+              'user_id' => $user->id,
+              'category_id' => $categoryOne->id,
+              'status_id' => $statusInProgress->id,
+          ]);
+  
+          Idea::factory()->create([
+              'user_id' => $user->id,
+              'category_id' => $categoryOne->id,
+              'status_id' => $statusInProgress->id,
+          ]);
+  
+          // $response = $this->get(route('idea.index', ['status' => 'In Progress']));
+          // $response->assertSuccessful();
+          // $response->assertSee('<div class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">In Progress</div>', false);
+          // $response->assertDontSee('<div class="bg-purple text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Considering</div>', false);
+  
+          Livewire::withQueryParams(['status' => 'In Progress'])
+              ->test(IdeasIndex::class)
+              ->assertViewHas('ideas', function ($ideas) {
+                  return $ideas->count() === 3
+                      && $ideas->first()->status->name === 'In Progress';
+            });
     }
 
     /** @test */
