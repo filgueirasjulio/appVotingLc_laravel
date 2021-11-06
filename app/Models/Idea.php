462421fcd783f014dde1a2dc23a5e134bdfa3aa8
Julio Filgueiras <julio_filgueiras@live.com>
2021-11-06 19:52:06 -0300

    feat: comments and ideas pagination

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -15,6 +15,7 @@ class Idea extends Model
     const PAGINATION_COUNT = 10;
 
     protected $guarded = [];
+    protected $perPage = 10;
 
     /**
      * Return the sluggable configuration array for this model.
